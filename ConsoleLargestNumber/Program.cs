ee322f243619e2537c3f0c41546b7fd82b123706
joshuapickenpaugh <joshuapickenpaugh@hotmail.com>
2017-07-27 11:53:58 -0500

    Sync.

--- ConsoleLargestNumber/Program.cs
+++ ConsoleLargestNumber/Program.cs
@@ -1,4 +1,8 @@
-﻿using System;
+﻿//Joshua Pickenpaugh
+//July 27th, 2017
+//Largest Number
+
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -10,6 +14,20 @@ namespace ConsoleLargestNumber
     {
         static void Main(string[] args)
         {
+            List<double> numList = new List<double>();
+
+            for (int x = 0; x < 10; x++)
+            {
+                Console.Write("Enter a decimal number:");
+                double dblEnteredNum = Convert.ToDouble(Console.ReadLine());
+                numList.Add(dblEnteredNum);
+            }
+
+            double dblHighNum = numList.Max();
+
+            Console.WriteLine("The largest number inputed was: {0}.", dblHighNum);
+            Console.WriteLine("Hit a key to EXIT.");
+            Console.ReadKey();
         }
     }
 }
